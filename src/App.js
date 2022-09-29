75a6f80ed94dd4ad47219c3e18c8511781df7e9d
MaxC0d3 <maximitianoreste@gmail.com>
2022-09-29 15:25:55 -0500

    Fix(styles): Se corrigieron estilos y grillas

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import HomeApp from './Components/HomeApp';
-import { PersonajesApp } from './Components/Personajes/PersonajesApp';
-import { PlanetasApp } from './Components/Planetas/PlanetasApp';
-import { DetallePersonaje } from './Components/Personajes/DetallePersonaje';
-import { DetallePlaneta } from './Components/Planetas/DetallePlaneta';
+import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import HomeApp from './Components/HomeApp'
+import { DetallePersonaje } from './Components/Personajes/DetallePersonaje'
+import { PersonajesApp } from './Components/Personajes/PersonajesApp'
+import { DetallePlaneta } from './Components/Planetas/DetallePlaneta'
+import { PlanetasApp } from './Components/Planetas/PlanetasApp'
 
 function App() {
   return (
     <div>
       <Routes>
-        <Route exact path='/' element={<HomeApp/>}></Route>
-        <Route exact path='/Personajes' element={<PersonajesApp/>} />
-        <Route exact path='/Planetas' element={<PlanetasApp/>}/>
-        <Route exact path='/Personaje/:name' element={<DetallePersonaje/>}/>
-        <Route exact path='/Planeta/:name' element={<DetallePlaneta/>}/>
+        <Route exact path='/' element={<HomeApp />}></Route>
+        <Route exact path='/Personajes' element={<PersonajesApp />} />
+        <Route exact path='/Planetas' element={<PlanetasApp />} />
+        <Route exact path='/Personaje/:name' element={<DetallePersonaje />} />
+        <Route exact path='/Planeta/:name' element={<DetallePlaneta />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
--- src/Components/NavbarApp/NavbarApp.js
+++ src/Components/NavbarApp/NavbarApp.js
@@ -1,33 +1,40 @@
-import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Navbar from 'react-bootstrap/Navbar';
-import Nav from 'react-bootstrap/Nav';
+import React from 'react'
+import Container from 'react-bootstrap/Container'
+import Nav from 'react-bootstrap/Nav'
+import Navbar from 'react-bootstrap/Navbar'
+import { FaGlobeAmericas, FaHome, FaUser } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
 import './NavbarApp.css'
-import { FaHome } from "react-icons/fa";
-import { FaUser } from "react-icons/fa";
-import { FaGlobeAmericas } from "react-icons/fa";
-import { Link } from 'react-router-dom';
 
 export const NavbarApp = () => {
   return (
-    <Navbar className='NavbarContainers' expand="lg">
+    <Navbar className='NavbarContainers' expand='lg'>
       <Container>
-        <Navbar.Brand href="#home" className='title_star'>
+        <Navbar.Brand href='#home' className='title_star'>
           <Link className='linkp' to='/'>
             Star Wars App
           </Link>
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
+        <Navbar.Toggle aria-controls='basic-navbar-nav' />
+        <Navbar.Collapse id='basic-navbar-nav'>
+          <Nav className='ms-auto'>
             <Nav.Link className='Textos'>
-              <Link className='linkto' to='/'><FaHome size={20} /> Inicio</Link>
+              <Link className='linkto' to='/'>
+                <FaHome size={20} />
+                Inicio
+              </Link>
             </Nav.Link>
             <Nav.Link className='Textos'>
-              <Link className='linkto' to='/Personajes'><FaUser size={20} /> Personajes</Link>
+              <Link className='linkto' to='/Personajes'>
+                <FaUser size={20} />
+                Personajes
+              </Link>
             </Nav.Link>
             <Nav.Link className='Textos'>
-              <Link className='linkto' to='/Planetas'><FaGlobeAmericas size={20} /> Planetas</Link>
+              <Link className='linkto' to='/Planetas'>
+                <FaGlobeAmericas size={20} />
+                Planetas
+              </Link>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
